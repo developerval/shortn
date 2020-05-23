2baef7f735ea51cda671c81b142d438a69202277
developerval <valentine.bott@gmail.com>
2020-05-23 22:02:54 +0100

    added basic layout for the page. need to finish form though, added second route for success in app.py, no logic yet.

---
+++ shortn/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/success')
+def success():
+    pass
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
