e2968350dded07d00a107709e055ca5ac7f9206a
developerval <valentine.bott@gmail.com>
2020-06-26 23:05:09 +0100

    added base of scheme checking function

--- shortn/link_utility.py
+++ shortn/link_utility.py
@@ -1,9 +1,12 @@
 import random
 import string
+from urllib.parse import urlparse
 
 def generate_link(chars=string.ascii_uppercase + string.ascii_lowercase + string.digits, N=5):
     return ''.join(random.choice(chars) for _ in range(N))
     
-def schema_check():
-    pass
+def schema_check(url):
+    result = urlparse(url)
+    if result.scheme == None:
+        return 'https://'
 # TODO: add code to check the scheme of a given link, if it doesn't have one, add https or http, maybe?
