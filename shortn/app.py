824ac9b960a7c7cb4f84bae6c0148efd6fd5f381
developerval <valentine.bott@gmail.com>
2020-06-30 23:36:01 +0100

    added database code to app file

--- shortn/app.py
+++ shortn/app.py
@@ -1,15 +1,33 @@
 from flask import Flask, make_response, jsonify, render_template, request, redirect
 from link_utility import generate_link
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
+
+class Data(db.Model):
+    __tablename__ = 'shortn'
+    id = db.Column(db.Integer, primary_key=True)
+    original_ = db.Column(db.String())
+    shortened_ = db.Column(db.string(5), unique=True)
+
+    def __init__(self, original_, shortened_):
+        self.original = original_
+        self.shortened = shortened_
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 @app.route('/success', methods=['POST', 'GET'])
 def success():
@@ -20,6 +38,7 @@ def success():
 
     res = make_response(jsonify(newone), 200)
     return res
+
 
 @app.route('/<url>')
 def forward(url):
